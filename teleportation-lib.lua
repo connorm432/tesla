8badd9d15ce3882ab9b4cfe1787622a2f081fa6b
connorm432 <113160834+connorm432@users.noreply.github.com>
2022-09-24 22:43:48 -0400

    Update teleportation-lib.lua

--- teleportation-lib.lua
+++ teleportation-lib.lua
@@ -184,7 +184,7 @@ function Module:Instant_Teleport(Destinition_Vector3, heart)
     local heart = heart or humanoid_root_part
 
     heart.CFrame = CFrame.new(Destinition_Vector3)
-		task.wait(12)
+		task.wait(10)
 
 end
 
